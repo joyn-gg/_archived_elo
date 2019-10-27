4708f40ef7e7c2aa0da1d19ef41b2423d4f94b6a
Passive <passivemodding@gmail.com>
2019-10-27 18:31:06 +1100

    added build files

---
+++ BuildUbuntu.bat
@@ -0,0 +1,2 @@
+dotnet publish ELO.csproj -c Release -r ubuntu.16.10-x64 -o ./bin/Release/ELO/Ubuntu
+pause
---
+++ BuildWindows.bat
@@ -0,0 +1,2 @@
+dotnet publish ELO.csproj -c Release -r win10-x64 -o ./bin/Release/ELO/Windows
+pause
