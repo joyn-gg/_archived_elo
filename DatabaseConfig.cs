8c96d70203a8a988a8377381e264e50d3e40c4de
Passive <passivemodding@gmail.com>
2019-10-31 12:31:35 +1100

    begun work on sql integration

---
+++ DatabaseConfig.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ELO
+{
+    public class DatabaseConfig
+    {
+        public string Username { get; set; }
+        public string Password { get; set; }
+        public string ServerName { get; set; }
+        public string DatabaseName { get; set; }
+    }
+}
