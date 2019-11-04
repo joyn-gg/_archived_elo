f4002d1706831f9be73641e05a566fbc736f2758
Passive <passivemodding@gmail.com>
2019-11-05 00:51:30 +1100

    Moved all necessary methods to gss

--- Services/GameSubmissionService.cs
+++ Services/GameSubmissionService.cs
@@ -282,5 +282,328 @@ namespace ELO.Services
             var embed = GameService.GetGameEmbed(game);
             await AnnounceResultAsync(context, lobby, embed);
         }
+
+
+        public async Task DrawAsync(ShardedCommandContext context, int gameNumber, SocketTextChannel lobbyChannel = null, [Remainder]string comment = null)
+        {
+            if (lobbyChannel == null)
+            {
+                //If no lobby is provided, assume that it is the current channel.
+                lobbyChannel = context.Channel as SocketTextChannel;
+            }
+
+            using (var db = new Database())
+            {
+                var lobby = db.GetLobby(lobbyChannel);
+                if (lobby == null)
+                {
+                    //Reply error not a lobby.
+                    await context.Channel.SendMessageAsync("", false, "Channel is not a lobby.".QuickEmbed(Color.Red));
+                    return;
+                }
+
+                var game = db.GameResults.Where(x => x.GuildId == context.Guild.Id && x.LobbyId == lobbyChannel.Id && x.GameId == gameNumber).FirstOrDefault();
+                if (game == null)
+                {
+                    //Reply not valid game number.
+                    await context.Channel.SendMessageAsync("", false, $"Game not found. Most recent game is {lobby.CurrentGameCount}".QuickEmbed(Color.DarkBlue));
+                    return;
+                }
+
+                if (game.GameState != GameState.Undecided)
+                {
+                    await context.Channel.SendMessageAsync("", false, "You can only call a draw on a game that hasn't been decided yet.".QuickEmbed(Color.Red));
+                    return;
+                }
+                game.GameState = GameState.Draw;
+                game.Submitter = context.User.Id;
+                game.Comment = comment;
+
+                db.Update(game);
+                db.SaveChanges();
+
+                await DrawPlayersAsync(context, db.GetTeamFull(game, 1), db);
+                await DrawPlayersAsync(context, db.GetTeamFull(game, 2), db);
+                await context.Channel.SendMessageAsync("", false, $"Called draw on game #{game.GameId}, player's game and draw counts have been updated.".QuickEmbed(Color.Green));
+                await AnnounceResultAsync(context, lobby, game);
+            }
+        }
+
+        public Task DrawPlayersAsync(ShardedCommandContext context, HashSet<ulong> playerIds, Database db)
+        {
+            foreach (var id in playerIds)
+            {
+                var player = db.Players.Find(context.Guild.Id, id);
+                if (player == null) continue;
+
+                player.Draws++;
+                db.Update(player);
+            }
+
+            db.SaveChanges();
+            return Task.CompletedTask;
+        }
+
+
+        private async Task GameVoteAsync(ShardedCommandContext context, Database db, Lobby lobby, GameResult game, int gameNumber, TeamSelection winning_team, HashSet<ulong> team1, HashSet<ulong> team2, [Remainder]string comment = null)
+        {
+            if (game.GameState == GameState.Decided || game.GameState == GameState.Draw)
+            {
+                await context.Channel.SendMessageAsync("", false, "Game results cannot currently be overwritten without first running the `undogame` command.".QuickEmbed(Color.Red));
+                return;
+            }
+
+            var competition = db.GetOrCreateCompetition(context.Guild.Id);
+            var ranks = db.Ranks.Where(x => x.GuildId == context.Guild.Id).ToArray();
+
+            List<(Player, int, Rank, RankChangeState, Rank)> winList;
+            List<(Player, int, Rank, RankChangeState, Rank)> loseList;
+            if (winning_team == TeamSelection.team1)
+            {
+                winList = UpdateTeamScoresAsync(competition, lobby, game, ranks, true, team1, db);
+                loseList = UpdateTeamScoresAsync(competition, lobby, game, ranks, false, team2, db);
+            }
+            else
+            {
+                loseList = UpdateTeamScoresAsync(competition, lobby, game, ranks, false, team1, db);
+                winList = UpdateTeamScoresAsync(competition, lobby, game, ranks, true, team2, db);
+            }
+
+            var allUsers = new List<(Player, int, Rank, RankChangeState, Rank)>();
+            allUsers.AddRange(winList);
+            allUsers.AddRange(loseList);
+
+            foreach (var user in allUsers)
+            {
+                //Ignore user updates if they aren't found in the server.
+                var gUser = context.Guild.GetUser(user.Item1.UserId);
+                if (gUser == null) continue;
+
+                await UserService.UpdateUserAsync(competition, user.Item1, ranks, gUser);
+            }
+
+            game.GameState = GameState.Decided;
+            game.WinningTeam = (int)winning_team;
+            game.Comment = comment;
+            game.Submitter = context.User.Id;
+            db.Update(game);
+            db.SaveChanges();
+
+            var winField = new EmbedFieldBuilder
+            {
+                //TODO: Is this necessary to show which team the winning team was?
+                Name = $"Winning Team, Team{(int)winning_team}",
+                Value = GetResponseContent(winList).FixLength(1023)
+            };
+            var loseField = new EmbedFieldBuilder
+            {
+                Name = $"Losing Team",
+                Value = GetResponseContent(loseList).FixLength(1023)
+            };
+            var response = new EmbedBuilder
+            {
+                Fields = new List<EmbedFieldBuilder> { winField, loseField },
+                //TODO: Remove this if from the vote command
+                Title = $"Game Id: {gameNumber}"
+            };
+
+            if (!string.IsNullOrWhiteSpace(comment))
+            {
+                response.AddField("Comment", comment.FixLength(1023));
+            }
+
+            await AnnounceResultAsync(context, lobby, response);
+        }
+
+        public async Task CancelAsync(ShardedCommandContext context, int gameNumber, SocketTextChannel lobbyChannel = null, [Remainder]string comment = null)
+        {
+            if (lobbyChannel == null)
+            {
+                //If no lobby is provided, assume that it is the current channel.
+                lobbyChannel = context.Channel as SocketTextChannel;
+            }
+
+            using (var db = new Database())
+            {
+                var lobby = db.GetLobbyWithQueue(lobbyChannel);
+                if (lobby == null)
+                {
+                    //Reply error not a lobby.
+                    await context.Channel.SendMessageAsync("", false, "Channel is not a lobby.".QuickEmbed(Color.Red));
+                    return;
+                }
+
+                var game = db.GameResults.Where(x => x.GuildId == context.Guild.Id && x.LobbyId == lobbyChannel.Id && x.GameId == gameNumber).FirstOrDefault();
+                if (game == null)
+                {
+                    //Reply not valid game number.
+                    await context.Channel.SendMessageAsync("", false, $"Game not found. Most recent game is {lobby.CurrentGameCount}".QuickEmbed(Color.DarkBlue));
+                    return;
+                }
+
+
+                if (game.GameState != GameState.Undecided && game.GameState != GameState.Picking)
+                {
+                    await context.Channel.SendMessageAsync("", false, $"Only games that are undecided or being picked can be cancelled.".QuickEmbed());
+                    return;
+                }
+
+
+
+                if (game.GameState == GameState.Picking)
+                {
+                    db.RemoveRange(lobby.Queue);
+                }
+                game.GameState = GameState.Canceled;
+                game.Submitter = context.User.Id;
+                game.Comment = comment;
+                db.Update(game);
+                db.SaveChanges();
+
+                await AnnounceResultAsync(context, lobby, game);
+            }
+        }
+
+        public async Task GameResultAsync(ShardedCommandContext context, int gameNumber, string voteState, SocketTextChannel lobbyChannel = null)
+        {
+            if (lobbyChannel == null)
+            {
+                lobbyChannel = context.Channel as SocketTextChannel;
+            }
+
+            //Do vote conversion to ensure that the state is a string and not an int (to avoid confusion with team number from old elo version)
+            if (int.TryParse(voteState, out var voteNumber))
+            {
+                await context.Channel.SendMessageAsync("", false, "Please supply a result relevant to you rather than the team number. Use the `Results` command to see a list of these.".QuickEmbed(Color.DarkBlue));
+                return;
+            }
+
+            if (!Enum.TryParse(voteState, true, out VoteState vote))
+            {
+                await context.Channel.SendMessageAsync("", false, "Your vote was invalid. Please choose a result relevant to you. ie. Win (if you won the game) or Lose (if you lost the game)\nYou can view all possible results using the `Results` command.".QuickEmbed(Color.Red));
+                return;
+            }
+
+            using (var db = new Database())
+            {
+                var lobby = db.GetLobby(lobbyChannel);
+                if (lobby == null)
+                {
+                    await context.Channel.SendMessageAsync("", false, "Current channel is not a lobby.".QuickEmbed(Color.Red));
+                    return;
+                }
+
+                var game = db.GameResults.SingleOrDefault(x => x.LobbyId == lobby.ChannelId && x.GameId == gameNumber);
+                if (game == null)
+                {
+                    await context.Channel.SendMessageAsync("", false, "Game not found.".QuickEmbed(Color.Red));
+                    return;
+                }
+
+                if (game.GameState != GameState.Undecided)
+                {
+                    await context.Channel.SendMessageAsync("", false, "You can only vote on the result of undecided games.".QuickEmbed(Color.Red));
+                    return;
+                }
+                else if (game.VoteComplete)
+                {
+                    //Result is undecided but vote has taken place, therefore it wasn't unanimous
+                    await context.Channel.SendMessageAsync("", false, "Vote has already been taken on this game but wasn't unanimous, ask an admin to submit the result.".QuickEmbed(Color.DarkBlue));
+                    return;
+                }
+
+                var team1 = db.GetTeamFull(game, 1);
+                var team2 = db.GetTeamFull(game, 2);
+
+                //TODO: Automatically submit if vote is from an admin.
+                if (team1.All(x => x != context.User.Id) && team2.All(x => x != context.User.Id))
+                {
+                    await context.Channel.SendMessageAsync("", false, "You are not a player in this game and cannot vote on it's result.".QuickEmbed(Color.Red));
+                    return;
+                }
+
+                var votes = db.Votes.Where(x => x.ChannelId == lobby.ChannelId && x.GameId == gameNumber).ToList();
+                if (votes.Any(x => x.UserId == context.User.Id))
+                {
+                    await context.Channel.SendMessageAsync("", false, "You already submitted your vote for this game.".QuickEmbed(Color.DarkBlue));
+                    return;
+                }
+
+                var userVote = new GameVote()
+                {
+                    UserId = context.User.Id,
+                    GameId = gameNumber,
+                    ChannelId = lobby.ChannelId,
+                    GuildId = context.Guild.Id,
+                    UserVote = vote
+                };
+
+                db.Votes.Add(userVote);
+                votes.Add(userVote);
+                db.SaveChanges();
+
+                //Ensure votes is greater than half the amount of players.
+                if (votes.Count * 2 > team1.Count + team2.Count)
+                {
+                    var drawCount = votes.Count(x => x.UserVote == VoteState.Draw);
+                    var cancelCount = votes.Count(x => x.UserVote == VoteState.Cancel);
+
+                    var team1WinCount = votes
+                                            //Get players in team 1 and count wins
+                                            .Where(x => team1.Contains(x.UserId))
+                                            .Count(x => x.UserVote == VoteState.Win)
+                                        +
+                                        votes
+                                            //Get players in team 2 and count losses
+                                            .Where(x => team2.Contains(x.UserId))
+                                            .Count(x => x.UserVote == VoteState.Lose);
+
+                    var team2WinCount = votes
+                                            //Get players in team 2 and count wins
+                                            .Where(x => team2.Contains(x.UserId))
+                                            .Count(x => x.UserVote == VoteState.Win)
+                                        +
+                                        votes
+                                            //Get players in team 1 and count losses
+                                            .Where(x => team1.Contains(x.UserId))
+                                            .Count(x => x.UserVote == VoteState.Lose);
+
+                    if (team1WinCount == votes.Count)
+                    {
+                        //team1 win
+                        await GameVoteAsync(context, db, lobby, game, gameNumber, TeamSelection.team1, team1.ToHashSet(), team2.ToHashSet(), "Decided by vote.");
+                    }
+                    else if (team2WinCount == votes.Count)
+                    {
+                        //team2 win
+                        await GameVoteAsync(context, db, lobby, game, gameNumber, TeamSelection.team2, team1.ToHashSet(), team2.ToHashSet(), "Decided by vote.");
+                    }
+                    else if (drawCount == votes.Count)
+                    {
+                        //draw
+                        await DrawAsync(context, gameNumber, lobbyChannel, "Decided by vote.");
+                    }
+                    else if (cancelCount == votes.Count)
+                    {
+                        //cancel
+                        await CancelAsync(context, gameNumber, lobbyChannel, "Decided by vote.");
+                    }
+                    else
+                    {
+                        //Lock game votes and require admin to decide.
+                        //TODO: Show votes by whoever
+                        await context.Channel.SendMessageAsync("", false, "Vote was not unanimous, game result must be decided by a moderator.".QuickEmbed(Color.DarkBlue));
+                        game.VoteComplete = true;
+                        db.Update(game);
+                        db.SaveChanges();
+                        return;
+                    }
+                }
+                else
+                {
+                    await context.Channel.SendMessageAsync("", false, $"Vote counted as: {vote.ToString()}".QuickEmbed(Color.Green));
+                }
+            }
+        }
     }
 }
