27b138cf13649be71d87c93032f7cfc01ad50247
Passive <passivemodding@gmail.com>
2020-01-06 21:43:10 +1100

    devcmd group for Dev module

--- Modules/Developer.cs
+++ Modules/Developer.cs
@@ -13,6 +13,7 @@ using System.Threading.Tasks;
 namespace ELO.Modules
 {
     [RavenRequireOwner]
+    [Group("devcmd")]
     public class Developer : ReactiveBase
     {
         public Developer(Random random, PremiumService prem, ConfigManager local)
