7c427584a0e93c732b8601d100bd28e932081f89
PassiveModding <passivemodding@gmail.com>
2020-02-10 17:09:52 +1100

    Add shardid to dscserializable

--- Models/DscSerializable.cs
+++ Models/DscSerializable.cs
@@ -10,6 +10,7 @@ namespace ELO.Models
         public LogSeverity LogLevel { get; set; } = LogSeverity.Info;
         public bool ExclusiveBulkDelete { get; set; } = true;
         public int Shards { get; set; } = 1;
+        public int? ShardId { get; set; } = null;
 
         public DiscordSocketConfig ToConfig()
         {
@@ -19,6 +20,7 @@ namespace ELO.Models
                 MessageCacheSize = this.MessageCacheSize,
                 LogLevel = this.LogLevel,
                 ExclusiveBulkDelete = this.ExclusiveBulkDelete,
+                ShardId = this.ShardId,
 
                 //You may want to edit the shard count as the bot grows more and more popular.
                 //Discord will block single shards that try to connect to more than 2500 servers
