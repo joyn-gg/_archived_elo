74eb09aa3d16fae785ffa6063f403a66fcea6c5a
PassiveModding <passivemodding@gmail.com>
2020-04-29 11:47:28 +1000

    Add db migration for KDR

--- src/Migrations/20200429014705_kdr.Designer.cs
+++ src/Migrations/20200429014705_kdr.Designer.cs
@@ -3,15 +3,17 @@ using System;
 using ELO.Services;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 
 namespace ELO.Migrations
 {
     [DbContext(typeof(Database))]
-    partial class DatabaseModelSnapshot : ModelSnapshot
+    [Migration("20200429014705_kdr")]
+    partial class kdr
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ src/Migrations/20200429014705_kdr.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace ELO.Migrations
+{
+    public partial class kdr : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "Deaths",
+                table: "Players",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<int>(
+                name: "Kills",
+                table: "Players",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Deaths",
+                table: "Players");
+
+            migrationBuilder.DropColumn(
+                name: "Kills",
+                table: "Players");
+        }
+    }
+}
