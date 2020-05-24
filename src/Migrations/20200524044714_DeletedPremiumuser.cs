2c4ed14d6f4380971befca100bdf99eede9e0602
PassiveModding <passivemodding@gmail.com>
2020-05-24 14:47:29 +1000

    Added migration for deletedPremiumUsers table

---
+++ src/Migrations/20200524044714_DeletedPremiumuser.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace ELO.Migrations
+{
+    public partial class DeletedPremiumuser : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "DeletedPremiumUsers",
+                columns: table => new
+                {
+                    UserId = table.Column<decimal>(nullable: false),
+                    EntitledRoleId = table.Column<decimal>(nullable: false),
+                    EntitledRegistrationCount = table.Column<int>(nullable: false),
+                    LastSuccessfulKnownPayment = table.Column<DateTime>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_DeletedPremiumUsers", x => x.UserId);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "DeletedPremiumUsers");
+        }
+    }
+}
