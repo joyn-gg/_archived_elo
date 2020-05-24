16529959a715ac49a98e66128886584d57c971b2
PassiveModding <passivemodding@gmail.com>
2020-05-24 16:28:16 +1000

    Add migration for dev whitelist table

--- src/Migrations/20200524062806_DevWhitelist.Designer.cs
+++ src/Migrations/20200524062806_DevWhitelist.Designer.cs
@@ -3,15 +3,17 @@ using System;
 using ELO.Services;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 
 namespace ELO.Migrations
 {
     [DbContext(typeof(Database))]
-    partial class DatabaseModelSnapshot : ModelSnapshot
+    [Migration("20200524062806_DevWhitelist")]
+    partial class DevWhitelist
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ src/Migrations/20200524062806_DevWhitelist.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace ELO.Migrations
+{
+    public partial class DevWhitelist : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "WhitelistedDevelopers",
+                columns: table => new
+                {
+                    UserId = table.Column<decimal>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_WhitelistedDevelopers", x => x.UserId);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "WhitelistedDevelopers");
+        }
+    }
+}
