69b1a966e95e471b5ce526bd4c6c8c4a0be8693d
PassiveModding <passivemodding@gmail.com>
2020-07-20 22:26:31 +1000

    Improve elojob efficiency

--- src/Services/ELOJobs.cs
+++ src/Services/ELOJobs.cs
@@ -13,6 +13,7 @@ namespace ELO.Services
         public ELOJobs(DiscordShardedClient client)
         {
             Client = client;
+
             //Setup a timer that fires every 5 minutes
             CompetitionUpdateTimer = new Timer(RunQueueChecks, null, 60000, 1000 * 60 * 5);
         }
@@ -20,6 +21,7 @@ namespace ELO.Services
         public DiscordShardedClient Client { get; }
 
         public Timer CompetitionUpdateTimer { get; }
+
         public void RunQueueChecks(object stateInfo = null)
         {
             var _ = Task.Run(async () =>
@@ -27,42 +29,42 @@ namespace ELO.Services
                 using (var db = new Database())
                 {
                     var now = DateTime.UtcNow;
-                    //TODO: Avoid querying ALL queued players
-                    // maybe query all competitions and check for the queuetimeout first, then query players as per each comp that has a timeout
-                    var queuedPlayers = db.QueuedPlayers.ToArray();
-                    var guildGroups = queuedPlayers.GroupBy(x => x.GuildId);
-                    foreach (var group in guildGroups)
-                    {
-                        var comp = db.GetOrCreateCompetition(group.Key);
-                        if (comp.QueueTimeout == null) continue;
 
-                        var lobbyQueues = group.GroupBy(x => x.ChannelId);
-                        foreach (var lobbyGroup in lobbyQueues)
+                    var competitions = db.Competitions.Where(x => x.QueueTimeout != null).ToArray();
+                    var compIds = competitions.Select(x => x.GuildId).ToArray();
+                    var affectedPlayers = db.QueuedPlayers.Where(x => compIds.Contains(x.GuildId)).ToArray();
+                    foreach (var competition in competitions)
+                    {
+                        var lobbyMembers = affectedPlayers.Where(x => x.GuildId == competition.GuildId).GroupBy(x => x.ChannelId).ToArray();
+                        foreach (var lobby in lobbyMembers)
                         {
-                            var lastGame = db.GameResults.Where(x => x.GuildId == group.Key && x.LobbyId == lobbyGroup.Key).OrderByDescending(x => x.GameId).FirstOrDefault();
+                            var lastGame = db.GameResults.Where(x => x.GuildId == competition.GuildId && x.LobbyId == lobby.Key).OrderByDescending(x => x.GameId).FirstOrDefault();
+
+                            // Do not remove players if the game is currently picking (since players remain in queue while picking is taking place)
                             if (lastGame != null && lastGame.GameState == GameState.Picking)
                             {
                                 continue;
                             }
 
-                            foreach (var player in lobbyGroup)
+                            foreach (var member in lobby)
                             {
                                 //Too much time has passed, user is to be removed from queue.
-                                if (player.QueuedAt + comp.QueueTimeout.Value < now)
+                                if (member.QueuedAt + competition.QueueTimeout.Value < now)
                                 {
                                     //Remove player from queue
-                                    db.QueuedPlayers.Remove(player);
+                                    db.QueuedPlayers.Remove(member);
+
                                     //Ensure lobby channel still exists and announce the user is removed from queue
-                                    var channel = Client.GetChannel(player.ChannelId) as SocketTextChannel;
+                                    var channel = Client.GetChannel(member.ChannelId) as SocketTextChannel;
                                     if (channel != null)
                                     {
                                         try
                                         {
-                                            await channel.SendMessageAsync(MentionUtils.MentionUser(player.UserId), false, $"{MentionUtils.MentionUser(player.UserId)} was removed from the queue as they have been queued for more than {comp.QueueTimeout.Value.GetReadableLength()}".QuickEmbed(Color.DarkBlue));
+                                            await channel.SendMessageAsync(MentionUtils.MentionUser(member.UserId), false, $"{MentionUtils.MentionUser(member.UserId)} was removed from the queue as they have been queued for more than {competition.QueueTimeout.Value.GetReadableLength()}".QuickEmbed(Color.DarkBlue));
                                         }
                                         catch //(Exception e)
                                         {
-                                            //   
+                                            //
                                         }
                                     }
                                 }
