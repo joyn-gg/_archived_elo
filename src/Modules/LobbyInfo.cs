0d1e34a1510ddc5323cd2d1d97da8174f6647a4d
PassiveModding <passivemodding@gmail.com>
2020-09-09 10:43:19 +1000

    hide captains from q when picking

--- src/Modules/LobbyInfo.cs
+++ src/Modules/LobbyInfo.cs
@@ -123,7 +123,12 @@ namespace ELO.Modules
                         var t1c = db.GetTeamCaptain(Context.Guild.Id, Context.Channel.Id, game.GameId, 1);
                         var t2c = db.GetTeamCaptain(Context.Guild.Id, Context.Channel.Id, game.GameId, 2);
                         var queue = db.GetQueue(game);
-                        var remainingPlayers = LobbyService.GetMentionList(LobbyService.GetUserList(Context.Guild, queue.Where(x => team1.All(y => y.UserId != x.UserId) && team2.All(y => y.UserId != x.UserId)).Select(x => x.UserId)));
+                        var remainingIds = queue.Where(x =>
+                            team1.All(y => y.UserId != x.UserId) &&
+                            team2.All(y => y.UserId != x.UserId) &&
+                            t1c.UserId != x.UserId &&
+                            t2c.UserId != x.UserId).Select(x => x.UserId);
+                        var remainingPlayers = LobbyService.GetMentionList(LobbyService.GetUserList(Context.Guild, remainingIds));
                         gameEmbed.AddField("Team 1", $"Captain: {MentionUtils.MentionUser(t1c.UserId)}\n{string.Join("\n", t1Users)}");
                         gameEmbed.AddField("Team 2", $"Captain: {MentionUtils.MentionUser(t2c.UserId)}\n{string.Join("\n", t2Users)}");
                         if (remainingPlayers.Any())
